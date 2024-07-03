ee34d2bd7ed0e723ab87bf3780be4862c72702b7
Mendes, Eric Silva <ericsilvaccp@gmail.com>
2024-07-02 21:41:45 -0300

    feat: add keyboard visible

--- lib/presentation/layouts/login/login_layout.dart
+++ lib/presentation/layouts/login/login_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:projectzeta/presentation/components/components.dart';
 
 class LoginLayout extends StatelessWidget {
@@ -7,19 +8,32 @@ class LoginLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
     var widthWithPadding = width * 0.05;
+    var heightWithPadding = height * 0.15;
 
-    return SafeArea(
-      child: Center(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: widthWithPadding),
-          child: const Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              LoginHeader(),
-              LoginForm(),
-              LoginFooter(),
-            ],
+    return KeyboardDismissOnTap(
+      child: KeyboardVisibilityBuilder(
+        builder: (context, visible) => Scaffold(
+          resizeToAvoidBottomInset: true,
+          body: SafeArea(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.only(
+                  left: widthWithPadding,
+                  right: widthWithPadding,
+                  top: visible ? 0 : heightWithPadding,
+                ),
+                child: const Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    LoginHeader(),
+                    LoginForm(),
+                    LoginFooter(),
+                  ],
+                ),
+              ),
+            ),
           ),
         ),
       ),
